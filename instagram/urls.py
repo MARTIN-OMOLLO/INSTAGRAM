17785abed04d9c21e5ab835ae7e99cbb407c74dd
MARTIN OMOLLO <martin.ochieng@student.moringaschool.com>
2022-04-02 16:52:36 +0300

    add account

--- instagram/urls.py
+++ instagram/urls.py
@@ -2,4 +2,5 @@ from django.contrib.auth import views
 urlpatterns = [
 #....
     url(r'^logout/$', views.logout, {"next_page": '/'}), 
+     url(r'^accounts/', include('registration.backends.simple.urls')),
 ] 
