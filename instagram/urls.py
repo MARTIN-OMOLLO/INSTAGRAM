5b5545bf32d950e089ec8b83f1aa049bc0bde435
MARTIN OMOLLO <martin.ochieng@student.moringaschool.com>
2022-04-02 23:28:38 +0300

    manage.py

--- instagram/urls.py
+++
@@ -1,6 +0,0 @@
-from django.contrib.auth import views 
-urlpatterns = [
-#....
-    url(r'^logout/$', views.logout, {"next_page": '/'}), 
-     url(r'^accounts/', include('registration.backends.simple.urls')),
-] 
