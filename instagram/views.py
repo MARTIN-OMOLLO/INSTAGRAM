189b23e79a6f59dc6da6d4992847c52a4be721e0
MARTIN OMOLLO <martin.ochieng@student.moringaschool.com>
2022-04-03 17:39:35 +0300

    add NewsDetails

--- instagram/views.py
+++ instagram/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required.
+from .forms import NewDetailForm
 
 # Create your views here.
 
-
-#.......
-
+#.....
 @login_required(login_url='/accounts/login/')
-def article(request, article_id):
-#.......
+def new_details(request,details_id):
+    current_user = request.user
+    if request.method == 'POST':
+        form = NewDetailsForm(request.POST, request.FILES)
+        if form.is_valid():
+            detail = form.save(commit=False)
+            detail.editor = current_user
+            detail.save()
+        return redirect('NewsToday')
+
+    else:
+        form = NewDetailsForm()
+    return render(request, 'new_details.html', {"form": form})
