ca7131cc5cf3385ce75b9f1ea57af7bc623b9fdd
MARTIN OMOLLO <martin.ochieng@student.moringaschool.com>
2022-04-03 17:13:07 +0300

    add  details

--- instagram/models.py
+++ instagram/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+#......
+class Article(models.Model):
+    title = models.CharField(max_length=60)
+    post = models.TextField()
+    editor = models.ForeignKey(User,on_delete=models.CASCADE)
+    tags = models.ManyToManyField(tags)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    article_image = models.ImageField(upload_to='articles/', blank=True)
