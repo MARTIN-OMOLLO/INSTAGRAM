755d47ce98fe39dfda614a2728358bed8ff51dc9
MARTIN OMOLLO <martin.ochieng@student.moringaschool.com>
2022-04-03 17:34:16 +0300

    add new post

--- instagram/urls.py
+++ instagram/urls.py
@@ -2,4 +2,5 @@ from django.contrib.auth import views
 urlpatterns = [
 #....
     url(r'^logout/$', views.logout, {"next_page": '/'}), 
+     url(r'^new/detail$', views.new_detail, name='new-detail')
 ] 
